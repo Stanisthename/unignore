da264b6e680d4b2406cdc16da613a55882c17da4
Stanisthename <stan@unignore.com>
2026-04-26 14:48:52 -0400

    Update AboutClient.js

--- components/about/AboutClient.js
+++ components/about/AboutClient.js
@@ -1,1 +1,13 @@
+import StorySection from "./StorySection";
 
+export default function AboutClient() {
+  return (
+    <div style={{
+      padding: "80px 40px",
+      maxWidth: 900,
+      margin: "0 auto"
+    }}>
+      <StorySection />
+    </div>
+  );
+}

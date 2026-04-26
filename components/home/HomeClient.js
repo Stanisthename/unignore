ab6e87ed8d9ac0ffbac8007957fd33f2133772ca
Stanisthename <stan@unignore.com>
2026-04-26 16:28:43 -0400

    Update HomeClient.js

--- components/home/HomeClient.js
+++ components/home/HomeClient.js
@@ -18,30 +18,34 @@ export default function HomeClient() {
 
   return (
     <div style={{ background: C.bg, color: C.text, minHeight: "100vh" }}>
-
       {/* NAV */}
-      <div style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        padding: "16px 24px",
-        background: scrolled ? "#0A0A0BEE" : "transparent",
-        borderBottom: scrolled ? `1px solid ${C.border}` : "none",
-        display: "flex",
-        justifyContent: "space-between",
-        zIndex: 1000
-      }}>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          padding: "16px 24px",
+          background: scrolled ? "#0A0A0BEE" : "transparent",
+          borderBottom: scrolled ? `1px solid ${C.border}` : "none",
+          display: "flex",
+          justifyContent: "space-between",
+          zIndex: 1000,
+        }}
+      >
         <div style={{ fontWeight: 700 }}>unignore</div>
         <a href="/about" style={{ color: C.muted, textDecoration: "none" }}>
           About
         </a>
       </div>
 
-      <Hero />
-      <TrustStrip />
-      <OfferStack />
-      <Footer />
+      {/* MAIN CONTENT */}
+      <div style={{ paddingTop: 80 }}>
+        <Hero />
+        <TrustStrip />
+        <OfferStack />
+        <Footer />
+      </div>
     </div>
   );
 }
